305926b17d7e1bd78b023a8de9af231526aedf5d
Artur Araújo <arturmeetcom@gmail.com>
2026-09-23 13:41:00 -0300

    Delete Atividade01.py

--- Atividade01.py
+++
@@ -1,8 +0,0 @@
-nivelAcesso = int(input("Digite seu nível de acesso: "))
-portaDestravada = False
-
-if nivelAcesso >= 5:
-    portaDestravada = True
-    print("Acesso liberado")
-else:
-    print("Acesso negado, Permissão insuficiente")
